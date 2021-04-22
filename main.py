5839d2d3ff104be34d0877a25687ce4f7c93baf0
Atharva Gangji <71313049+AtharvaGangji@users.noreply.github.com>
2021-04-22 10:59:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# --------------------------------------- imports
+import pygame
+
+
+# --------------------------------------- game funtion
+def play():
+  pass
+
+# --------------------------------------- needed if statement
+if __name__ == "__main__":
+  play()
