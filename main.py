afccb5a57553a04a9d90d4cd419b709d06c21051
Atharva Gangji <71313049+AtharvaGangji@users.noreply.github.com>
2021-04-27 10:13:36 +0530

    just made the structure of the game.

--- main.py
+++ main.py
@@ -2,10 +2,29 @@
 import pygame
 
 
-# --------------------------------------- game funtion
+# --------------------------------------- game function
 def play():
-  pass
+    pygame.init()
+
+    # ---------------- Game Window
+    window = pygame.display.set_mode((500, 500))
+    window.fill((20, 222, 30))
+    pygame.display.update()
+
+    # ---------------- Game Loop
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            # ---------- Quit Window
+            if event.type == pygame.QUIT:
+                running = False
+            # ---------- Escape Button Quit Window
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
 
 # --------------------------------------- needed if statement
 if __name__ == "__main__":
-  play()
+    play()
