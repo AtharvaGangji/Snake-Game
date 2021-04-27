5ac0521f84e87d97bf774bd5a3870ba9a586b928
Atharva Gangji <71313049+AtharvaGangji@users.noreply.github.com>
2021-04-27 20:35:15 +0530

    did the snake part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # --------------------------------------- imports
 import pygame
+import time
 
 
 # --------------------------------------- game function
@@ -11,18 +12,61 @@ def play():
     window.fill((20, 222, 30))
     pygame.display.update()
 
+    # ---------------- variables
+    X = 100
+    Y = 100
+    direction = ""
+    time_sleep = 0.04
+
+    # ---------------- Snake
+    snake = pygame.image.load("resources/block.jpg").convert()  # load image
+
+    # ---------------- snake position
+    def snake_position():
+        window.fill((20, 222, 30))  # fill background with defined colour
+        window.blit(snake, (X, Y))  # draw image on the screen
+        pygame.display.update()  # update the screen
+    snake_position()
+
     # ---------------- Game Loop
     running = True
 
     while running:
+
+        # ---------------- Move snake in some direction
+        if direction == "UP":
+            Y -= 10
+            time.sleep(time_sleep)  # time sleep
+            snake_position()
+        elif direction == "DOWN":
+            Y += 10
+            time.sleep(time_sleep)  # time sleep
+            snake_position()
+        elif direction == "LEFT":
+            X -= 10
+            time.sleep(time_sleep)  # time sleep
+            snake_position()
+        elif direction == "RIGHT":
+            X += 10
+            time.sleep(time_sleep)  # time sleep
+            snake_position()
+
         for event in pygame.event.get():
             # ---------- Quit Window
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:  # Quit window if close button pressed
                 running = False
-            # ---------- Escape Button Quit Window
+            # ---------- Track Buttons
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+                if event.key == pygame.K_ESCAPE:  # Escape Button Quit Window
                     running = False
+                if event.key == pygame.K_UP:  # Key up move snake
+                    direction = "UP"
+                if event.key == pygame.K_DOWN:  # Key down move snake
+                    direction = "DOWN"
+                if event.key == pygame.K_RIGHT:  # Key down move snake
+                    direction = "RIGHT"
+                if event.key == pygame.K_LEFT:  # Key down move snake
+                    direction = "LEFT"
 
 
 # --------------------------------------- needed if statement
